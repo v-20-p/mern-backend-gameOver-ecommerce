8612827ce4e9b921afc03f01c944906e5670d638
khalid <10abha10@gmail.com>
2023-12-12 23:57:39 +0300

    db fixed

--- api/index.ts
+++ api/index.ts
@@ -45,8 +45,10 @@ app.use((req, res, next) => {
 })
 app.use(apiErrorHandler)
 
+// app.listen(PORT, () => {
+//   console.log('Server running http://localhost:' + PORT)
 
-
+// })
 
   
 export default app
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,10 @@
-import moogoose from 'mongoose'
+import mongoose from 'mongoose'
 import { dev } from '.'
 
 export const connectDB = async () => {
   try {
-    await moogoose.connect(dev.db.url)
+    await mongoose.set('strictQuery', true);
+    await mongoose.connect(dev.db.url)
   } catch (error) {
     console.error(error)
   }
