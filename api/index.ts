edb7cbd9f975cfa0d14013c931f986509ab694fb
khalid <10abha10@gmail.com>
2024-03-08 17:49:21 +0300

    v org 6

--- api/index.ts
+++ api/index.ts
@@ -19,6 +19,7 @@ config()
 export const app: Application = express()
 //app.use(morgan('dev'));
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin',  'http://localhost:*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   
